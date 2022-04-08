2b4182087a63c3e9932e37ec2b68029f750dcf0a
StarPandaBeg <kshmurakov@mail.ru>
2022-04-08 15:05:18 +0300

    V2.0.6 Backward support for 1.0.x versions

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# Backport to 1.0
+if (__name__ != "__main__"):
+    def main():
+        import os
+        if (os.path.exists("update.py")):
+            os.remove("update.py")
+            with open("run.bat", 'r', encoding="utf-8") as f:
+                content = f.read()
+            with open("run.bat", 'w', encoding="utf-8") as f:
+                f.write(content.replace("update.py", "main.py"))
+            os.system(f"run.bat")
+            exit()
+
 from func import *
 
 load_paths()
